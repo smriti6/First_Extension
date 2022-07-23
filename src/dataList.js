558d89b28dfb65b4e11c17d647a313652a2850ee
smriti6 <smritis481@gmail.com>
2022-07-23 21:10:50 +0530

    project

---
+++ src/dataList.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        F1:"a1",
+        F2:"First try"
+    },
+    {
+        F1:"b1",
+        F2:"Second go"
+    },
+    {
+        F1:"C1",
+        F2:"getting started"
+    },
+    {
+        F1:"a2",
+        F2:"cool people"
+    },
+    {
+        F1:"s2",
+        F2:"awa"
+    },
+    {
+        F1:"s1",
+        F2:"shiva"
+    }
+]
